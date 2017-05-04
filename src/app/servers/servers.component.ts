1709022b9f23e7e46709bbb11b1f4053acecdcae
Khalid Said <kerbrose@hotmail.com>
2017-05-04 13:41:25 +0200

    Section 2 Lecture 28

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() { 
     setTimeout(() => {
@@ -21,6 +22,11 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created';
+  }
+  
+  onUpdateServerName(event:Event){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
