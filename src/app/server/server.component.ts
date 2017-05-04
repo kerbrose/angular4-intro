6e3bf2f13e56f8fd5826da54a6e9f3cd828af144
Khalid Said <kerbrose@hotmail.com>
2017-05-04 02:49:41 +0200

    Section 2 Lecture 26

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,8 @@ import { Component} from '@angular/core';
 export class ServerComponent{
     serverID = 10;
     serverStatus = 'offline';
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },5000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created';
   }
 
 }
